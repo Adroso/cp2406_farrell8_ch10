8635313793bdac7656e3b9ef4b869c3bbaaa4703
Adroso360 <adrian.lapico@my.jcu.edu.au>
2016-09-05 10:40:05 +1000

    Exersize 3

---
+++ Excersises/Poems/DemoPoems.java
@@ -0,0 +1,7 @@
+package Poems;
+
+/**
+ * Created by Adroso360 on 5/09/2016.
+ */
+public class DemoPoems {
+}
---
+++ Excersises/Selling/ItemSold.java
@@ -0,0 +1,35 @@
+package Selling;
+
+/**
+ * Created by Adroso360 on 5/09/2016.
+ */
+public class ItemSold
+{
+    private int invoiceNumber;
+    private String description;
+    private double price;
+    public void setInvoiceNumber(int num)
+    {
+        invoiceNumber = num;
+    }
+    public void setDescription(String desc)
+    {
+        description = desc;
+    }
+    public void setPrice(double pr)
+    {
+        price = pr;
+    }
+    public int getInvoiceNumber()
+    {
+        return invoiceNumber;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+    public double getPrice()
+    {
+        return price;
+    }
+}
