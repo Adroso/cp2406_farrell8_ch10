e44b2cdad789dfb355212800f04f367cd0908d45
Adroso360 <adrian.lapico@my.jcu.edu.au>
2016-09-05 10:34:02 +1000

    Set up Exersize 2

--- Excersises/Candle/ScentedCandle.java
+++ Excersises/Candle/ScentedCandle.java
@@ -3,5 +3,22 @@ package Candle;
 /**
  * Created by Adroso360 on 5/09/2016.
  */
-public class ScentedCandle {
+public class ScentedCandle extends Candle
+{
+    private String scent;
+    public String getScent()
+    {
+        return scent;
+    }
+    public void setScent(String scent)
+    {
+        this.scent = scent;
+    }
+    @Override
+    public void setHeight(int h)
+    {
+        final double PER_INCH = 3;
+        super.setHeight(h);
+        price = h * PER_INCH;
+    }
 }
